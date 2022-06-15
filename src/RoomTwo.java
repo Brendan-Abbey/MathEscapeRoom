967743e024a4dfd92bf8bf0f846350a2ea6b9685
Brendan-Abbey <brendan8abb@gmail.com>
2022-06-15 12:39:45 -0400

    Fixed the answer out so that if answer is correct it says it is

--- src/RoomTwo.java
+++ src/RoomTwo.java
@@ -11,6 +11,7 @@
 public class RoomTwo extends javax.swing.JFrame {
     public static int i = 0;
     public static boolean cor = true;
+    public static int ans;
     /**
      * Creates new form RoomTwo
      */
@@ -38,6 +39,7 @@ public class RoomTwo extends javax.swing.JFrame {
         ansout = new javax.swing.JLabel();
         q1 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        test = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -70,7 +72,7 @@ public class RoomTwo extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(265, 265, 265)
+                        .addGap(231, 231, 231)
                         .addComponent(jButton1))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(20, 20, 20)
@@ -128,13 +130,19 @@ public class RoomTwo extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(79, 79, 79)
+                .addComponent(test)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(78, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(test)
+                .addContainerGap(72, Short.MAX_VALUE))
         );
 
         pack();
@@ -142,14 +150,15 @@ public class RoomTwo extends javax.swing.JFrame {
  
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        int b = i;
-        if (b != 3) {
+        
+        if (i != 3) {
             Exponents e = new Exponents();
             String s = "";
             s += e.getBase() + " ^ " + e.getExponent();
             q1.setText(String.valueOf(s));
             int t = Integer.parseInt(ansbar1.getText());
-            if (e.getAnswer()== t) {
+            test.setText(String.valueOf(e.getAnswer()));
+            if (ans== t) {
                 cor = true;
                 i++;
                 ansout.setText(String.valueOf("You got the last question correct!"));
@@ -157,7 +166,8 @@ public class RoomTwo extends javax.swing.JFrame {
                 cor = false;
                  ansout.setText(String.valueOf("You got the last question incorrect"));
             }
-            Key1.setText(String.valueOf(b));
+            Key1.setText(String.valueOf(i));
+            ans = e.getAnswer();
            }
         else{
             Start ret = new Start();
@@ -219,6 +229,7 @@ public class RoomTwo extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel q1;
+    private javax.swing.JLabel test;
     // End of variables declaration//GEN-END:variables
 
     //Exponents method for questions
